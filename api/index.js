364a4fc15cee4bdc36aa643f3c0f7443d5ab25e1
MuhammadBabarWaseem <mbabarwaseem@gmail.com>
2023-05-15 20:11:37 +0500

    upload image from  machine as well

--- api/index.js
+++ api/index.js
@@ -7,6 +7,8 @@ const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs');
 const cookieParser = require('cookie-parser');
 const imageDownloader = require('image-downloader');
+const multer = require('multer');
+const fs = require('fs');
 require('dotenv').config()
 
 const bcryptSalt = bcrypt.genSaltSync(10);
@@ -77,16 +79,30 @@ app.post('/logout', (req, res) => {
     res.cookie('token', '').json(true);
 })
 
-console.log({__dirname})
 app.post('/upload-by-link', async (req, res) => {
     const { link } = req.body;
-    const newName ='photo' + Date.now() + '.jpg'
+    const newName = 'photo' + Date.now() + '.jpg'
     await imageDownloader.image({
         url: link,
         dest: __dirname + '\\uploads\\' + newName,
     });
     res.json(newName);
+})
 
+const photoMiddleware = multer({dest:'uploads/'});
+app.post('/upload', photoMiddleware.array('photos', 50), (req, res) => {
+    const uploadedFiles = [];
+    for (let i = 0; i < req.files.length; i++) {
+        const { path, originalname } = req.files[i];
+        const parts = originalname.split('.');
+        const ext = parts[parts.length - 1];
+        const newPath = path + '.' + ext;   
+        fs.renameSync(path, newPath);
+        uploadedFiles.push(newPath.replace("uploads\\" , ""));
+
+
+    }
+    res.json(uploadedFiles);
 })
 
 
--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -20,9 +20,27 @@ const PlacesPage = () => {
         e.preventDefault();
         const { data: filename } = await axios.post('/upload-by-link', { link: photoLink });
         setAddedPhotos(prev => {
-            return [...prev, filename]
+            return [...prev, filename];
         });
         setPhotoLink('');
+    }
+
+    const uploadPhoto = (e) => {
+        const files = e.target.files;
+        const data = new FormData();
+
+        for (let i = 0; i < files.length; i++) {
+            data.append('photos', files[i]);
+        }
+
+        axios.post('/upload', data, {
+            headers: { 'Content-type': 'multipart/form-data' }
+        }).then(response => {
+            const { data: filenames } = response;
+            setAddedPhotos(prev => {
+                return [...prev, ...filenames]
+            });
+        });
     }
 
 
@@ -59,18 +77,20 @@ const PlacesPage = () => {
                             <input type="text" value={photoLink} onChange={e => setPhotoLink(e.target.value)} placeholder={'Add Images Using A Link .......jpg'} />
                             <button onClick={addPhotoByLink} className='bg-gray-200 px-4 rounded-2xl'>Add&nbsp;Photo</button>
                         </div>
+
                         <div className='mt-2 grid gap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-6'>
                             {addedPhotos.length > 0 && addedPhotos.map((link, index) => (
                                 <div className='' key={index}>
                                     <img className='rounded-2xl w-40 h-40' src={'http://localhost:4000/uploads/' + link} alt="Dummy" />
                                 </div>
                             ))}
-                            <button className="flex items-center gap-1 justify-content border bg-transparent rounded-2xl p-8 text-2xl text-gray-600">
+                            <label className="cursor-pointer flex items-center gap-1 justify-content border bg-transparent rounded-2xl p-8 text-2xl text-gray-600">
+                                <input type="file" className='hidden' multiple onChange={uploadPhoto} />
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
                                 </svg>
                                 Upload
-                            </button>
+                            </label>
                         </div>
 
                         <h2 className='text-xl mt-4'>Description</h2>
