265cc904b35d0ef2246f1639b2144fede200ee2d
MuhammadBabarWaseem <mbabarwaseem@gmail.com>
2023-05-17 13:00:48 +0500

    Delete photo option added as well. on edit and create place

--- client/src/pages/PhotosUploader.jsx
+++ client/src/pages/PhotosUploader.jsx
@@ -33,6 +33,10 @@ const PhotosUploader = ({ addedPhotos, onChange }) => {
         });
     }
 
+    const removePhoto = (filename) => {
+        onChange([...addedPhotos.filter(photo => photo !== filename)]);
+    }
+
     return (
         <>
             <h2 className='text-xl mt-4'>Photos</h2>
@@ -46,16 +50,16 @@ const PhotosUploader = ({ addedPhotos, onChange }) => {
                 {addedPhotos && addedPhotos.length > 0 && addedPhotos.map(link => (
                     <div className='h-32 relative flex' key={link}>
                         <img className='rounded-2xl w-full object-cover' src={'http://localhost:4000/uploads/' + link} alt="Dummy" />
-                        <div className='absolute bottom-1 right-2 py-1 px-1 text-white bg-black bg-opacity-50 rounded-2xl'>
+                        <button onClick={() => removePhoto(link)} className='cursor-pointer absolute bottom-1 right-2 py-1 px-1 text-white bg-black bg-opacity-50 rounded-2xl'>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                             </svg>
-                        </div>
+                        </button>
                     </div>
                 ))}
                 <label className="h-32 cursor-pointer flex items-center gap-1 justify-content border bg-transparent rounded-2xl p-8 text-2xl text-gray-600">
                     <input type="file" className='hidden' multiple onChange={uploadPhoto} />
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-9 h-9">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
                     </svg>
                     Upload
